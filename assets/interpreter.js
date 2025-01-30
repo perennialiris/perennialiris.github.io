ee6210f648e76288df4ae2e50f13bfaa101a2b1e
Iris Embury <irisembury@gmail.com>
2025-01-30 18:56:07 -0500

    Initial commit

---
+++ assets/interpreter.js
@@ -0,0 +1,322 @@
+
+/*  I want a list at the bottom of each page that shows all the addresses linked
+    to throughout the article. Instead of doing this manually, I have the
+    code that parses links also push them to this array, which is then used to
+    populate a citations list at the end automatically (see layout.js) */
+let citation_array = [];
+
+/*  I like being able to style numbers (0-9) separately from other text. However,
+    you don't want to affect numbers that are inside <elements>, because those
+    are often part of URLs or styling. This function takes an HTML element
+    and wraps digits in a span, but safely skips over the content of <tags>. */
+
+/*
+String.prototype.indexOf = function (string_, character_) {
+    for (let num = 0; num < string_.length; num += 1) if (string_[num] === character_) return num; return -1;
+}
+*/
+function wrapDigits(targetElement) {
+    let input = targetElement.innerHTML, output = "";
+    while (true) {
+        const openTag = input.indexOf("<"), closeTag = input.indexOf(">");
+        if (openTag == -1 || closeTag == -1) { break; }
+        output += input.substring(0, openTag).replace(/(\d+)/g, "<span class='rendered_digit'>$1</span>")
+            + input.substring(openTag, closeTag + 1);
+        input = input.substring(closeTag + 1);
+    }
+    output += input.replace(/(\d+)/g, "<span class='rendered_digit'>$1</span>");
+    targetElement.innerHTML = output;
+}
+
+/*  These are the replacements run over all inputs, separated into its
+    own function because I needed to call it multiple times. */
+function stdReplacements(input_string) {
+    if (input_string == "") {
+        return ""; }
+    return input_string
+        .replaceAll("\\*", "&ast;")
+        .replaceAll("---", "&mdash;")
+        .replaceAll("...", "&hellip;")
+        .replaceAll("--", "&ndash;")
+        .replaceAll("\\", "&#92;")
+        /*  you need these ][)( escapes for when putting links on the page, if you want
+            to use ][ as part of the text or link to a URL that has )( in it */
+        .replaceAll("\\(", "&lpar;")
+        .replaceAll("\\)", "&rpar;")
+        .replaceAll("\\[", "&lbrack;")
+        .replaceAll("\\]", "&rbrack;")
+        /* bold and italics */
+        .replace(/\*\*(.+?)\*\*/g, "<b>$1</b>")
+        .replace(/\*(.+?)\*/g, "<i>$1</i>")
+        /* curly quotes: */
+        .replace(/(\s|^|\[)"/g, "$1&ldquo;")
+        .replace(/(\W)"(\w)/g, "$1&ldquo;$2")
+        .replace(/"/g, "&rdquo;")
+        .replace(/(\s|^|\[)'/g, "$1&lsquo;")
+        .replace(/(\W)'(\w)/g, "$1&lsquo;$2")
+        .replace(/'/g, "&rsquo;");
+}
+
+/* This is a general parser I run all input through. It safely ignores
+   anything <inside> of tags, with the same logic as wrapDigits */
+function safeConvert(input_string) {
+    let input = input_string, output = "";
+    while (true) {
+        let openTag = input.indexOf("<"), closeTag = input.indexOf(">");
+        if (openTag == -1 || closeTag == -1) break;
+        output += stdReplacements(input.substring(0, openTag)) + input.substring(openTag, closeTag + 1);
+        input = input.substring(closeTag + 1);
+    }
+    output += stdReplacements(input);
+    return output;
+}
+
+/*  This one is for <code> and <div class="codeblock"> elements, where you
+    don't want any formatting to apply. Run this before safeConvert */
+function cleanForCode(input_string) {
+    return input_string
+        .replaceAll("=\"\"", "")
+        .replaceAll("\"", "&quot;")
+        .replaceAll("'", "&apos;")
+        .replaceAll("-", "&hyphen;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll("(", "&lpar;")
+        .replaceAll(")", "&rpar;")
+        .replaceAll("[", "&lbrack;")
+        .replaceAll("]", "&rbrack;")
+        .replaceAll("*", "&ast;")
+        .replaceAll("\n", "<br>");
+}
+/*  .replaceAll is preferred over .replace when you don't need to regex
+    functionality because it simply results in more readable code */
+
+
+
+/*  ![description](path/to/image.png)
+    ![nice](path/to/other_image.png)
+ ->v
+    <div class="image-box">
+        <div><img alt="description" title="description" src="path/to/image.png"></div>
+        <div><img alt="nice" title="nice" src="path/to/other_image.png"></div>
+    </div>                                                                     */
+function imageBoxParse(input_string) {
+    let lines = input_string.split("\n");
+    for (let i = 0; i < lines.length; i += 1) {
+        const parts = lines[i].split("]("); if (parts.length != 2) { console.error("Image parser is confused (bad input)"); break; }
+        const altText = parts[0].substring(2).replaceAll('"', '&quot;');
+        let filePath = parts[1];
+        let maxHeight = 350;
+        /* defaults to 350 max height unless specified otherwise - ![](images/1-1.png)|211 would have 211 max height */
+        let j = filePath.indexOf("|");
+        if (j != -1) {
+            maxHeight = filePath.substring(j + 1);
+            filePath = filePath.substring(0, j - 1); }
+        while (filePath.charAt(filePath.length - 1) == ")") {
+            filePath = filePath.substring(0, filePath.length - 1); }
+        lines[i] = (altText == "")
+            ? `<div><img onclick="imageViewer(this)" style="max-height:${maxHeight}px" src="${filePath}"></div>`
+            : `<div><img onclick="imageViewer(this)" style="max-height:${maxHeight}px" src="${filePath}" title="${altText}" alt="${altText}"></div>`;
+    }
+    return `<div class="image-box">${lines.join("")}</div>`;
+}
+
+
+/* nested list parser */
+function listParser(inputString) {
+    const items = inputString.split("\n");
+    const end_tags = [];
+    const indent_diff = [];
+    for (let j = 0; j < items.length; j += 1) {
+        let k = 0;
+        while (items[j].charAt(k) === " ") {
+            k += 1; }
+        items[j] = `${" ".repeat(k)}<li>${items[j].slice(k).replace(/^[\*\-]\s+/, "- ").trimEnd()}</li>`; }
+    for (let j = 0; j < items.length; j += 1) {
+        let indent = items[j].indexOf("<");
+        const prev_indent = (j > 0) ? items[j - 1].indexOf("<") : -1;
+        const li = items[j].slice(indent + 4);
+        
+        if (indent > prev_indent) {
+            if (li.startsWith("- ")) {
+                end_tags.push("</ul>");
+                items[j] = "<ul><li>" + li.slice(2); }
+            else {
+                if (/^\d+\./.test(li)) {
+                    end_tags.push("</ol>");
+                    items[j] = `<ol start="${li.substring(0, li.indexOf("."))}"><li>${li.slice(li.indexOf(".") + 1).trimStart()}`; }
+                else {
+                    items[j] = "<br>" + " ".repeat(indent - prev_indent) + li;
+                    items[j - 1] = items[j - 1].slice(0, -5);
+                    indent = prev_indent; }
+            indent_diff.push(indent - prev_indent); } }
+        else {
+            let br = false;
+            if (li.startsWith("- ")) {
+                items[j] = "<li>" + li.slice(2); }
+            else {
+                if (/^\d+\./.test(li)) {
+                    items[j] = "<li>" + li.slice(li.indexOf(".") + 1).trimStart(); }
+                else {
+                    br = true; } }
+            if (indent < prev_indent) {
+                let diff = prev_indent - indent;
+                while (diff > 0) {
+                    if (diff < indent_diff[indent_diff.length - 1]) {
+                        indent_diff[indent_diff.length - 1] -= diff;
+                        diff = 0;
+                    } else {
+                        diff -= indent_diff.pop();
+                        items[j - 1] += end_tags.pop(); } } }
+            if (br) {
+                let k;
+                for (k = j - 1; k > -1; k -= 1) {
+                    if (items[k].indexOf("<") == indent) {
+                        break; }
+                    if (items[k].indexOf("<") < indent) {
+                        k += 1; break; } }
+                items[k] = items[k].slice(0, -5);
+                items[j] = "<br>" + li; } }
+        items[j] = " ".repeat(indent) + items[j]; }
+    while (end_tags.length > 0) {
+        items[items.length - 1] += end_tags.pop(); }
+    return items.join('\n');
+}
+
+/* The main interpreter loop. Pass the main article element to start. */
+function interpreter(targetElement) {
+    let input = targetElement.innerHTML
+        .replace(/\n\n+/g, "\n\n")
+        .replace(/\r/g, "") /* safety */
+        .trim()
+        .split("\n\n");
+    
+    let table_num = 1;
+    for (let i = 0; i < input.length; i += 1) {
+        if (input[i].startsWith("\\")) {
+            input[i] = input[i].substring(1);
+            continue; }
+        let smallPrint = false;
+        if (input[i].startsWith("^")) {
+            smallPrint = true;
+            input[i] = input[i].substring(1); }
+        if (input[i] == "***" || input[i] == "---") {
+            input[i] = "<hr>";
+            continue; }
+        if (input[i] == "**" || input[i] == "--") {
+            input[i] = "<br>";
+            continue; }
+        if (input[i].startsWith("![")) {
+            input[i] = imageBoxParse(input[i]);
+            continue; }
+        
+        /* before looking for code, fix any \` instances: */
+        input[i] = input[i].replace(/\\`/g, "&#96;");
+        /* div.codeblock: */
+        if (input[i].startsWith("```")) {
+            input[i] = input[i].replace(/\s*```\n*/g, "");
+            input[i] = "<div class=\"codeblock\">" + cleanForCode(input[i]) + "</div>";
+            continue; }
+        /* <code></code>: */
+        input[i] = input[i].replace(/`(.+?)`/g, (match, captureGroup) => {
+            return "<code>" + cleanForCode(captureGroup) + "</code>"; });
+        
+        /* ------------------------ links ------------------------- */
+        input[i] = input[i].replace(/\[([^\]]*?)\]\((.+?)\)/g, (match, displayText, address) => {
+            let index = citation_array.indexOf(address);
+            if (index == -1) index = citation_array.push(address);
+            return (displayText === "")
+                ? `<a class="citeref" target="_blank" href="${address}">[${index}]</a>`
+                : `<a target="_blank" href="${address}">${displayText}</a>`; });
+        
+        /* ------------------------ table ------------------------- */
+        if (input[i].startsWith("|table")) {
+            let rows = input[i].split("\n"); rows.shift();
+            for (let j = 0; j < rows.length; j += 1) {
+                let cells = rows[j].split("|");
+                for (let k = 0; k < cells.length; k += 1) {
+                    cells[k] = "<td>" + cells[k].trim() + "</td>"; }
+                rows[j] = "<tr>" + cells.join("") + "</tr>"; }
+            input[i] = `<table id="${"table" + table_num++}" class="noq-table">${safeConvert(rows.join(""))}</table>`;
+            continue; }
+        
+        input[i] = safeConvert(input[i]);
+        
+        /* ------------------------ headers ------------------------ */
+        /* ------ h1 ------ */
+        if (input[i].startsWith("# ")) {
+            /* .title-box */
+            /* strict format requirement: "# title text |dddd-dd-dd" */
+            if (/\|(\d{4})-(\d{2})-(\d{2})/.test(input[i].substring(input[i].length - 11))) {
+                let date = input[i].slice(input[i].length - 10);
+                let title = input[i].slice(2, input[i].length - 11).trim();
+                input[i] = `<div class="title-box"><h1>${title}</h1><span class="date-box">date first posted: ${date}</span></div>`;
+                title = title.replace(/<\/?(i|b)>/g, "").replace(/&amp;/g, "&");
+                if (document.title == "") { document.title = title; } }
+            else {
+                let title = input[i].slice(2);
+                let titleId = title.replace(/<\/?(i|b)>/g, "");
+                input[i] = `<h1 id="${titleId}">${title}</h1>`; }
+            continue; }
+        /* ------ h2 ------ */
+        if (input[i].startsWith("## ")) {
+            let title = input[i].slice(3);
+            let titleId = title.replace(/<\/?(i|b)>/g, "");
+            input[i] = `<h2 id="${titleId}">${title}</h2>`;
+            continue; }
+        /* ------ h3 ------ */
+        if (input[i].startsWith("### ")) {
+            let title = input[i].slice(3);
+            input[i] = "<h3>" + title + "</h2>";
+            continue; }
+        
+        /* ------------ table of contents (not a table) ----------- */
+        if (input[i].startsWith("|toc")) {
+            let lines = input[i].split("\n"); lines.shift();
+            for (let j = 0; j < lines.length; j += 1) {
+                lines[j] = `<li><a href="#${lines[j]}">${lines[j]}</a></li>`; }
+            input[i] = "<ul class=\"toc\">" + lines.join("") + "</ul>";
+            continue; }
+        
+        /* ---------------------- blockquote ---------------------- */
+        if (input[i].startsWith("&gt;")) {
+            let lines = input[i].split("\n");
+            for (let j = 0; j < lines.length; j += 1) {
+                lines[j] = lines[j].replace(/^&gt;/, "").trim();
+                if (lines[j].startsWith("&mdash;")) {
+                    lines[j] = "<div class=\"attribution\">" + lines[j] + "</div>"; }
+                else {
+                    if (lines[j].startsWith("^")) {
+                        lines[j] = "<div class=\"fine\">" + lines[j].substring(1) + "</div>"; }
+                    else {
+                        lines[j] += "\n"; } } }
+            input[i] = lines.join("")
+                .replace(/\n\n/g, "<br class=\"bq-br\">")
+                .replace(/\n/g, "<br>");
+            input[i] = "<blockquote>" + input[i] + "</blockquote>";
+            continue; }
+        input[i] = input[i].replaceAll("&mdash;", "<span class=\"mdash\">&mdash;</span>");
+        
+        /* ------------------------ lists ------------------------- */
+        if (input[i].startsWith("* ") || input[i].startsWith("- ") || /^\d+\./.test(input[i])) {
+            input[i] = listParser(input[i]);
+            if (smallPrint) {
+                if (input[i].substring(0, 3) != "<ol" && input[i].substring(0, 3) != "<ul") console.error("{INTERPRETER.JS: (B).}")
+                input[i] = input[i].substring(0, 3) + " class=\"fine\"" + input[i].substring(3); }
+            continue;
+        }
+        input[i] = input[i].replace(/\n/g, "<br>");
+        if (input[i] == "") { console.error("{INTERPRETER.JS: (C).}"); }
+        
+        input[i] = (smallPrint)
+            ? "<p class=\"fine\">" + input[i] + "</p>"
+            : "<p>" + input[i] + "</p>";
+    }
+    targetElement.innerHTML = input.join("");
+    
+    function foo(y) { let x = targetElement.getElementsByTagName(y); for (let k = 0; k < x.length; k += 1) { x[k] = wrapDigits(x[k]); } }
+    foo("p");
+    foo("li");
+    foo("blockquote");
+}
+
---
+++ assets/layout.js
@@ -0,0 +1,122 @@
+
+let index_data = `
+2025-events-log | 2025 news | pinned |
+
+31 | Reflections on Justin Trudeau                | politics | 2025-01-19
+32 | Political conservatism                       | politics | 2025-01-21
+24 | Enduring falsehoods about Warren, Clinton    | politics | 2024-12-19
+27 | Sex, gender, & transsexuals                  | politics | 2025-01-02
+7  | Fetishism & politics                         | politics | 2024-11-14
+14 | Reasons I’m glad to be Canadian              | politics | 2024-12-08
+13 | The military industrial complex              | politics | 2024-12-04
+11 | The Trump appeal                             | politics | 2024-12-03
+12 | The order of information                     | politics | 2024-12-03
+6  | Mark Robinson                                | politics | 2024-11-13
+9  | The default politician                       | politics | 2024-11-26
+2  | The trans prison stats argument              | politics | 2024-10-19
+22 | Dehumanization                               | politics | 2024-12-15
+
+29 | Date formats                                 | other | 2025-01-11
+10 | Touchscreens and smartphones                 | other | 2024-12-02
+
+1  | Language                                     | personal | 2024-10-29
+30 | The appearance of intelligence               | personal | 2025-01-18
+25 | A beauty holding a bird                      | personal | 2024-12-23
+8  | 10 Dollar                                    | personal | 2024-11-25
+28 | Therapy theory                               | personal | 2025-01-09
+21 | Relationships                                | personal | 2024-12-14
+4  | Anime reviews                                | personal | 2024-11-02
+3  | Poor things (2023 film)                      | personal | 2024-10-31
+5  | Types of masculinity                         | personal | 2024-11-08
+
+`;
+
+function main() {
+    const top_ = document.getElementById("top"); if (!top_) { console.error("{LAYOUT.JS: Can't find #top}"); return; }
+    top_.innerHTML = `<header id="header"><div><a href="index.html"><img height="67" width="252" alt="North of Queen logo" src="assets/header-image.png"></a></div></header><div id="nav"></div>`;
+    document.head.innerHTML += `<link rel="stylesheet" href="assets/main.css"><link rel="icon" type="image/x-icon" href="assets/favicon.ico">`;
+    const footer = document.body.appendChild(document.createElement("footer"));
+    footer.id = "footer";
+    footer.innerHTML += `<div id="image-viewer-wrapper" onclick="closeImageViewer()"><img id="image-viewer"></div>`;
+    
+    /* If you want a right panel: */
+    // const contentWrapper = document.getElementById("content-wrapper"); if (!contentWrapper) { console.error("{LAYOUT.JS: Can't find #content-wrapper}"); }
+    // contentWrapper.appendChild(document.createElement("div"));
+    
+    const main = document.getElementById("main"); if (!main) { console.error("{LAYOUT.JS: Can't find #main}"); }
+    const article = document.getElementById("article");
+    if (article) {
+        interpreter(article);
+        
+        let article_footer = main.appendChild(document.createElement("footer"));
+        article_footer.id = "article-footer";
+        article_footer.innerHTML = `<p>North of Queen is a personal webpage. I’m not part of any organization.</p>`;
+        
+        if (citation_array) {
+            if (citation_array.length > 0) {
+                for (let i = 0; i < citation_array.length; i += 1) {
+                    citation_array[i] = `<li><a href="${citation_array[i]}">${citation_array[i]}</a></li>`; }
+                let citations = main.appendChild(document.createElement("div"));
+                citations.id = "citations";
+                citations.innerHTML = `<div>external resources linked above:</div><ol>${citation_array.join("")}</ol>`; }
+        }
+    }
+    
+    const sidebar_data = { "pinned" : [], "politics": [], "other": [], "personal": [] };
+    index_data = index_data.split("\n");
+    for (let i = 0; i < index_data.length; i += 1) {
+        const cell = index_data[i].trim().split("|").map(data => data.trim());
+        if (cell.length == 4) {
+            let file = cell[0],
+            title    = cell[1],
+            category = cell[2],
+            date     = cell[3];
+            if (!sidebar_data[category]) { continue; }
+            let extras = (category == "pinned") ? `<img src="assets/pin2.png" height="15" width="15">` : "";
+            let other_attributes = (title == document.title) ? `class="current-page"` : "";
+            let entry = `<div><a ${other_attributes} href="${file}.html">${title}</a>${extras}</div>`;
+            sidebar_data[category].push(entry); }
+    }
+    
+    const sidebar = document.getElementById("sidebar"); if (!sidebar) { console.error("{LAYOUT.JS: Can't find #sidebar}"); }
+    if (sidebar) {
+        sidebar.innerHTML = `
+        ${sidebar_data.pinned.join("")}
+        <h3>politics</h3>
+        ${sidebar_data.politics.join("")}
+        <h3>other</h3>
+        ${sidebar_data.other.join("")}
+        ${sidebar_data.personal.join("")}
+        `;}
+    
+    
+    document.title += " - North of Queen";
+    const cover = document.getElementById("cover");
+    if (cover) {
+        cover.classList.add("fade-out");
+        cover.addEventListener("animationend", () => {
+            cover.remove(); }); }
+}
+
+let viewerWrapper;
+let viewerImg;
+function imageViewer(img) {
+    viewerWrapper = document.getElementById("image-viewer-wrapper");
+    viewerImg = document.getElementById("image-viewer");
+    viewerWrapper.style.display = "flex";
+    viewerImg.src = img.src;
+    viewerImg.alt = img.alt;
+}
+function closeImageViewer() {
+    viewerWrapper = document.getElementById("image-viewer-wrapper");
+    viewerImg = document.getElementById("image-viewer");
+    viewerWrapper.style.display = "none";
+    viewerImg.src = "";
+    viewerImg.alt = "";
+}
+
+window.onload = main;
+
+
+
+
