668f8dd5db58d53fcc75a6bc9a402d12e4447fb8
Iris Embury <irisembury@gmail.com>
2025-03-04 08:46:48 -0500

    index adjustments

--- assets/layout.js
+++ assets/layout.js
@@ -4,24 +4,31 @@
 function get(x) { let temp = document.getElementById(x); if (temp == null) { console.error(`document.getElementById("${x}") returned null`); } return temp; }
 
 let data = `
-33 | The Lorax sux | culture | 2025-02-27 | narrow
-23 | Passing | transgender culture | 2025-02-24 |
-20 | Israel–Palestine notes | politics | 2025-02-24 | toc
-19 | Ilhan Omar’s comments about Somalia | politics | 2025-02-12 |
-17 | Why get bottom surgery? | transgender, culture | 2025-02-09 |
+39 | Movies | | | narrow unlisted
+38
+37 | Bluesky accounts listing | other | | wide toc
+36 | India | | | unlisted
 35 | Show and tell (Lex Fridman) | politics | 2025-02-05 |
-16 | Milo Yiannopoulos’s cancellation | politics | 2025-02-03 |
 34 | The Nazi salute | politics | 2025-01-24 | narrow
+33 | The Lorax sux | culture | 2025-02-27 | narrow
+32 | Conservatism | politics | 2025-01-05 | wide toc
+31 | Reflections on Justin Trudeau | politics | 2025-01-08 |
 30 | The appearance of intelligence | other | 2025-01-18 |
 29 | Date formats | other | 2025-01-11 | narrow
 28 | Therapy theory | personal | 2025-01-09 |
-31 | Reflections on Justin Trudeau | politics | 2025-01-08 |
-32 | Conservatism | politics | 2025-01-05 | wide toc
 27 | Sex, gender, & transsexuals | transgender, politics | 2024-12-29 | toc
+26 | News 2025 | politics | | wide pinned
 25 | A beauty holding a bird | other | 2024-12-23 | narrow
 24 | Enduring falsehoods about Warren, Clinton | politics | 2024-12-19 |
+23 | Passing | transgender, culture | 2025-02-24 |
 22 | Dehumanization | politics | 2024-12-15 |
 21 | Relationships | personal | 2024-12-14 |
+20 | Israel–Palestine notes | politics | 2025-02-24 | toc
+19 | Ilhan Omar’s comments about Somalia | politics | 2025-02-12 |
+18 | Transcripts: context for inflammatory Trump statements | politics | |
+17 | Why get bottom surgery? | transgender, culture | 2025-02-09 |
+16 | Milo Yiannopoulos’s cancellation | politics | 2025-02-03 |
+15 | Mark Robinson transcript | | 2024-11-13 | unlisted
 14 | Reasons I’m glad to be Canadian | politics | 2024-12-08 |
 13 | The military–industrial complex | politics | 2024-12-04 |
 12 | The order of information | politics | 2024-12-03 |
@@ -29,22 +36,25 @@ let data = `
 10 | Touchscreens and smartphones | culture | 2024-12-02 |
 9 | The default politician | politics | 2024-11-26 |
 8 | 10 Dollar | culture | 2024-11-25 |
-7 | Fetishism & politics | transgender culture | 2024-11-14 |
-15 | Mark Robinson transcript | | 2024-11-13 | unlisted
+7 | Fetishism & politics | transgender, culture | 2024-11-14 |
 6 | Mark Robinson | politics | 2024-11-13 |
 5 | Types of masculinity | culture | 2024-11-08 |
 4 | Anime reviews | culture | 2024-11-02 |
 3 | Poor things (2023 film) | culture | 2024-10-31 |
-1 | Language | personal | 2024-10-29 |
 2 | The trans prison stats argument | transgender, politics | 2024-10-19 |
-39 | Movies | | | narrow unlisted
-37 | Bluesky accounts listing | other | | wide toc
-36 | People don’t really have world views | | | unlisted
-26 | News 2025 | politics | | wide pinned
-18 | Transcripts: context for inflammatory Trump statements | politics | |
+1 | Language | personal | 2024-10-29 |
 index | | | | unlisted narrow
 `;
-let imgGallery = [[`assets/gallery/j-c-dahl-frogner-manor-1842.jpg`, `Frogner Manor (1842) by J. C. Dahl` ],[ `assets/gallery/j-c-dahl-copenhagen-harbor-by-moonlight-1839.jpg`, `Copenhagen Harbor by Moonlight (1846) by J. C. Dahl` ],[ `assets/gallery/frederic-edwin-church-niagara-1857.jpg`, `Niagara (1857) by Frederic Edwin Church.jpg` ],[ `assets/gallery/frederic-edwin-church-mt-ktaadn-1853.jpg`, `Mt. Ktaadn (1853) by Frederic Edwin Church` ],[ `assets/gallery/cole-thomas-the-consummation-1836.jpg`, `The Consummation (1836) by Cole Thomas` ],[ `assets/gallery/yosemite-national-park.jpg`, `Yosemite National Park (unknown).jpg` ],[ `assets/gallery/karl-friedrich-schinkel-landschaft-mit-pilger-1813.jpg`, `Landschaft mit Pilger (1813) by Karl Friedrich Schinkel` ]];
+
+let imgGallery = [
+`src="assets/gallery/j-c-dahl-frogner-manor-1842.jpg" alt="Frogner Manor (1842) by J. C. Dahl"`,
+`src="assets/gallery/j-c-dahl-copenhagen-harbor-by-moonlight-1839.jpg" alt="Copenhagen Harbor by Moonlight (1846) by J. C. Dahl"`,
+`src="assets/gallery/frederic-edwin-church-niagara-1857.jpg" alt="Niagara (1857) by Frederic Edwin Church.jpg"`,
+`src="assets/gallery/frederic-edwin-church-mt-ktaadn-1853.jpg" alt="Mt. Ktaadn (1853) by Frederic Edwin Church"`,
+`src="assets/gallery/cole-thomas-the-consummation-1836.jpg" alt="The Consummation (1836) by Cole Thomas"`,
+`src="assets/gallery/yosemite-national-park.jpg" alt="Yosemite National Park (unknown).jpg"`,
+`src="assets/gallery/karl-friedrich-schinkel-landschaft-mit-pilger-1813.jpg" alt="Landschaft mit Pilger (1813) by Karl Friedrich Schinkel"`
+];
 /*     38    40    */
 
 var lbKeyResponsive = false;
@@ -63,18 +73,19 @@ function closeLightbox() {
     lightboxImg.alt = ""; }
 
 function alignTable(dataString, splitChar) {
-    let table = dataString.split("\n").filter(c => c != "").map(row => row.split(splitChar).map(cell => cell.trim()));
-    let tableWidth = Math.max(...table.map(row => row.length));
-    for (let col = 0; col < tableWidth; col += 1) {
+    const table = dataString.split("\n").filter(c => c.split(splitChar).length == 5).map(row => row.split(splitChar).map(cell => cell.trim()));
+    const rowWidth = Math.max(...table.map(row => row.length));
+    for (let col = 0; col < rowWidth; col += 1) {
         let cellWidth = 0;
         for (let i = 0; i < table.length; i += 1) {
-            while (table[i].length < tableWidth) {
+            while (table[i].length < rowWidth) {
                 table[i].push(""); }
             if (table[i][col].length > cellWidth) {
                 cellWidth = table[i][col].length; } }
         for (let i = 0; i < table.length; i += 1) {
-            if (table[i].length < tableWidth) { continue; }
-            table[i][col] += " ".repeat(cellWidth - table[i][col].length); } }
+            if (table[i].length < rowWidth) { continue; }
+            table[i][col] += " ".repeat(cellWidth - table[i][col].length);
+            } }
     table.sort((a, b) => {
         a = a[3].replace(/\D/g, ""); if (a == "") { a = 0; }
         b = b[3].replace(/\D/g, ""); if (b == "") { b = 0; }
@@ -113,16 +124,15 @@ function tocHighlighter() {
 
 function getFileName() {
     const u = document.baseURI.split("/").slice(-1)[0];
-    const v = u.indexOf("#");
-    const w = v == -1 ? u : u.substring(0, v);
-    return w.replace(/\.html$/, "");
+    const i = u.indexOf("#");
+    const r = i == -1 ? u : u.substring(0, i);
+    return r.replace(/\.html$/, "");
 }
 
 function pageLoad() {
     document.head.innerHTML += `<link rel="icon" type="image/x-icon" href="assets/favicon.ico"><link rel="stylesheet" href="assets/main.css">`;
 
     const fileName = getFileName();
-    const randomImg = imgGallery[Math.floor(Math.random() * imgGallery.length)];
 
     get("page").innerHTML =
        `<header id="header"><a href="index.html"><img height="67" width="252" alt="North of Queen logo" src="assets/header-image.png"></a></header>
@@ -134,7 +144,7 @@ function pageLoad() {
                     <div>Find me on: <a target="_blank" href="https://bsky.app/profile/irispol.bsky.social">Bluesky</a> | <a target="_blank" href="https://northofqueen.substack.com">Substack</a> | <a target="_blank" href="https://forthoseinterested.tumblr.com">Tumblr</a> | <a target="_blank" href="https://discord.com/invite/puJEP8HKk3">Discord</a></div>
                 </section>
                 <footer id="footer">
-                    <div><img style="max-width:100%" src="${randomImg[0]}" alt="${randomImg[1]}"></div>
+                    <div><img style="max-width:100%" ${imgGallery[Math.floor(Math.random() * imgGallery.length)]}></div>
                     <div><a target="_blank" href="https://github.com/northofqueen">North of Queen</a> is my personal repo. I have no association with any other person or organization. Code uploaded to this repo (northofqueen) can be interpreted as fully public domain (<a href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank">CC0</a>). I also give broad permission for my writing to be used, reposted, etc. for non-commercial purposes provided no other person claims authorship (<a href="https://creativecommons.org/licenses/by-nc/4.0/" target="_blank">CC BY-NC 4.0</a>).</div>
                 </footer>
             </div>
@@ -157,27 +167,25 @@ function pageLoad() {
     let includeToc = false;
     const dataRows = data.split("\n");
     for (let i = 0; i < dataRows.length; i += 1) {
-        let cells = dataRows[i].split("|").map(cell => cell.trim());
-        if (cells.length >= 5) {
-            let rowFile = cells[0],
-            rowTitle =    cells[1],
-            rowCategory = cells[2],
-            rowDate =     cells[3],
-            rowFlags =    cells[4].split(" ");
-            if (rowFile == fileName) {
-                if (rowFlags.includes("toc")) { includeToc = true; }
-                if (rowFlags.includes("wide")) { get("page").classList.add("wide"); }
-                else if (rowFlags.includes("narrow")) { get("page").classList.add("narrow"); }
-                }
-            if (rowFlags.includes("unlisted")) { continue; }
-            const isPinned = (rowFlags.includes("pinned"));
-            let entryClass = "nav-row";
-            if (isPinned) { entryClass += " pinned"; }
-            if (pageList.recent.length < 8) { pageList.recent.push(`<a href="${rowFile}.html">${rowTitle}</a>`); }
-            let indexEntry = `<li><a href="${rowFile}.html">${rowTitle}${isPinned?`<img class="icon" src="assets/pin2.png" height="17" width="17">` : ''}</a> <b>·</b> <span>${rowCategory}</span>${rowDate != '' ? ' <b>·</b> <span>'+rowDate+'</span>' : ''}</li>`;
-            if (isPinned) { pageList.full.unshift(indexEntry); }
-            else { pageList.full.push(indexEntry); }
-        }
+        const cells = dataRows[i].split("|").map(cell => cell.trim());
+        const rowFile = cells[0],
+        rowTitle    = cells[1],
+        rowCategory = cells[2],
+        rowDate     = cells[3],
+        rowFlags    = cells[4].split(" ");
+        if (rowFile == fileName) {
+            if (rowFlags.includes("toc")) { includeToc = true; }
+            if (rowFlags.includes("wide")) { get("page").classList.add("wide"); }
+            else if (rowFlags.includes("narrow")) { get("page").classList.add("narrow"); }
+            }
+        if (rowFlags.includes("unlisted")) { continue; }
+        const isPinned = (rowFlags.includes("pinned"));
+        let entryClass = "nav-row";
+        if (isPinned) { entryClass += " pinned"; }
+        if (pageList.recent.length < 8) { pageList.recent.push(`<a href="${rowFile}.html">${rowTitle}</a>`); }
+        let indexEntry = `<li><a href="${rowFile}.html">${rowTitle}${isPinned?`<img class="icon" src="assets/pin2.png" height="17" width="17">` : ''}</a> <b>·</b> <span>${rowCategory}</span>${rowDate != '' ? ' <b>·</b> <span>'+rowDate+'</span>' : ''}</li>`;
+        if (isPinned) { pageList.full.unshift(indexEntry); }
+        else { pageList.full.push(indexEntry); }
     }
 
     const indexTable = document.getElementById("page-list");
